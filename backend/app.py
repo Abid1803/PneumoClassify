7dc9d666efefd4583f08e90a782426e991472e6a
Mohammad Abid <abid455.md@gmail.com>
2025-11-27 17:29:32 +0530

    fixing the errors

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import tensorflow as tf
 import cv2
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = tf.keras.models.load_model("pneumonia_model.h5")
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
-
     arr = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(arr, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (224, 224))
